0c05621bf52757d1430c82335e512c391b2ef277
chiranz <chiranpoudyal@gmail.com>
2022-05-09 06:59:10 +0530

    build: add scripts to package.json

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,7 @@ module.exports = {
   },
   plugins: ["@typescript-eslint"],
   extends: [
-    "standard",
+    "eslint:recommended",
     "plugin:prettier/recommended",
     "plugin:node/recommended",
   ],
