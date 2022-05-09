e855b455ea68ad39731c831fda2b6729d765c153
chiranz <chiranpoudyal@gmail.com>
2022-05-09 09:59:22 +0530

    setup: update eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,18 @@
 module.exports = {
   env: {
-    browser: false,
     es2021: true,
-    mocha: true,
     node: true,
   },
-  plugins: ["@typescript-eslint"],
   extends: [
     "eslint:recommended",
-    "plugin:prettier/recommended",
-    "plugin:node/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: 12,
     sourceType: "module",
   },
-  rules: {
-    "node/no-unsupported-features/es-syntax": [
-      "error",
-      { ignores: ["modules"] },
-    ],
-  },
+  plugins: ["@typescript-eslint"],
+  rules: {},
 };
