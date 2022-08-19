4d772e699bce46773cab29653f20de547c4b6544
chiranz <chiranpoudyal@gmail.com>
2022-08-19 09:33:29 +0530

    script: add vArmor to armor multiplying factor

--- scraper/createRoot.ts
+++ scraper/createRoot.ts
@@ -5,6 +5,9 @@ import { BigNumber } from "ethers";
 import BalanceTree from "../test/helpers/balance-tree";
 
 (async function () {
+  // TODO: Update this conversion rate
+  const conversionRate = BigNumber.from(10).pow(18);
+  const scalingFactor = BigNumber.from(10).pow(18);
   //   write all the details to a json file
   // fetch  balanceNodes
   const balanceNodesPath = path.resolve(
@@ -21,6 +24,7 @@ import BalanceTree from "../test/helpers/balance-tree";
       depositStart: BigNumber.from(balNode.depositStart),
       amount: BigNumber.from(balNode.amount),
     };
+    node.amount = node.amount.mul(conversionRate).div(scalingFactor);
     balanceNodes.push(node);
   }
   // create tree
